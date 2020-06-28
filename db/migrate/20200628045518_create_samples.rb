f465826a2713331d86c6ea65f2fe1adc1a317e40
栄 俊介 <sakaes@SO-SAKAES-MacBook-Pro.local>
2020-06-28 14:00:31 +0900

    webpacker導入

---
+++ db/migrate/20200628045518_create_samples.rb
@@ -0,0 +1,10 @@
+class CreateSamples < ActiveRecord::Migration[5.2]
+  def change
+    create_table :samples do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
